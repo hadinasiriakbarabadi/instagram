37ac2646e14a8f382da480f51b563dd15f5d6301
hadinasiri <hadinasiri.6970@gmail.com>
2023-11-02 00:59:34 +0330

    .

---
+++ lib/igbot.py
@@ -0,0 +1,19 @@
+from instagrapi import Client
+
+class InstagramBot:
+    def __init__(self,username,password):
+        self.username=username
+        self.password=password
+        self.client=Client()
+    def sign_in(self):
+        self.client.login(self.username,self.password)
+    def sign_out(self):
+        self.client.logout()
+    def post(self,image,caption):
+        self.client.photo_upload(image,caption)
+    def post_album(self,caption,images):
+        self.client.album_upload(images,caption)
+ig_bot=InstagramBot('username','password')
+ig_bot.sign_in()
+ig_bot.post('image.jpg','caption')
+
